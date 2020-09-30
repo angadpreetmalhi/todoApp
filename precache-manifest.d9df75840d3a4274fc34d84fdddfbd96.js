ed39cdfd8f21819ff71225f5c419cc0987e3cba9
Angad Preet Singh <angadpreetmalhi@gmail.com>
2020-09-30 03:16:31 -0700

    Updates

---
+++ precache-manifest.d9df75840d3a4274fc34d84fdddfbd96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "126316e9754de959de16b7bd4d949cc8",
+    "url": "/todoApp/index.html"
+  },
+  {
+    "revision": "feac558660162d7a9c30",
+    "url": "/todoApp/static/css/main.ede93482.chunk.css"
+  },
+  {
+    "revision": "4012d908433370307007",
+    "url": "/todoApp/static/js/2.96526db2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todoApp/static/js/2.96526db2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feac558660162d7a9c30",
+    "url": "/todoApp/static/js/main.1606eec6.chunk.js"
+  },
+  {
+    "revision": "df7629b979bd038184bb",
+    "url": "/todoApp/static/js/runtime-main.29991ed7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todoApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
