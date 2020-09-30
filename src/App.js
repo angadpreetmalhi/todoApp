8f4fb85bbf81f813fc7fca64061f5a50b6f5a39e
Angad Preet Singh <angadpreetmalhi@gmail.com>
2020-09-30 02:44:30 -0700

    first react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import React from 'react';
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ListCom from './ListCom';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+
 
 function App() {
+  const [item,setItem]=useState("Add an Item");
+  const [newItem,setNewItem]=useState([]);
+  const itemEvent=(event)=>{
+  setItem(event.target.value)
+  }
+  const listOfItems =()=>{
+  setNewItem((preValue)=>{
+  return[...preValue,item];
+  });
+  setItem("");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <>
+    <div className="main_div">
+      <div className="center_div">
+      <br/>
+      <h1>ToDo List</h1>
+      <br/>
+      <input type="text" value={item}
+      placeholder="Add an Item" onChange={itemEvent}/>
+      <Button className="newBtn" onClick={listOfItems}>
+      <AddIcon/>
+      </Button>
+      <br/>
+      <ol>
+      {
+      newItem.map((val)=>{ return <ListCom text={val}/>;})
+      }
+      </ol>
+      </div>
     </div>
+    </>
   );
 }
 
